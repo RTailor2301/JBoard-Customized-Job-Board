cce45c081d7d8ef2586d8d8633c244da28e495fd
rt-524 <rt524@njit.edu>
2025-10-11 13:35:35 -0400

    added validation logic in create

--- public_html/M4/todos/create.php
+++ public_html/M4/todos/create.php
@@ -22,8 +22,15 @@ if (empty($diff)) {
     
     // rt524 10/11/25 
     // check if due is 8 chars long (already in DATE format) and if assigned is empty/blank
-
-
+    if (strlen($due) != 10) {
+        echo "Date is not valid. Please provide a valid date.";
+        $is_valid = false;
+    }
+    if (empty(trim($assigned))) {
+        echo "Assigned is not valid. Defaulting to 'self'";
+        $assigned = "self";
+        $is_valid = false;
+    }
     // End validations
 
     
