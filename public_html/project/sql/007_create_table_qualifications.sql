0613135dcea2e99778377c8875597186ae7f3dc2
rt-524 <rt524@njit.edu>
2025-11-22 16:43:09 -0500

    adjusted table names

--- public_html/project/sql/007_create_table_qualifications.sql
+++ public_html/project/sql/007_create_table_qualifications.sql
@@ -1,8 +1,8 @@
-CREATE TABLE `Jsearch_Qualifications` (
+CREATE TABLE IF Not EXISTS `IT202_F25_Jsearch_Qualifications` (
     `id`         INT AUTO_INCREMENT PRIMARY KEY,
     `job_id`       VARCHAR(128) NOT NULL,
     `qualification_text` TEXT NOT NULL,
 
-    FOREIGN KEY (`job_id`) REFERENCES `IT202_S25_Jsearch`(`job_id`)
+    FOREIGN KEY (`job_id`) REFERENCES `IT202_F25_Jsearch`(`job_id`)
         ON DELETE CASCADE
 )
